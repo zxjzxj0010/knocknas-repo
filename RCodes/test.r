1a4f6e250bb9830e736595945d6d65ffdd72f9d4
zxjzxj0010 <zxjzxj0010@126.com>
2026-02-12 20:56:28 +0800

    重构 RCodes，删除 main.r 文件并添加 test.r 文件，包含计算和绘图示例

---
+++ RCodes/test.r
@@ -0,0 +1,27 @@
+library(tidyverse)
+# 1、计算和输出
+
+add <- function(x, y) {
+    x + y
+}
+
+print(add(1, 2))
+print(add(1.0e10, 2.0e10))
+print(paste("one", NULL))
+print(paste(NA, "two"))
+print(paste("multi-line", "multi-line"))
+
+# 2、绘图
+
+h <- c(1, 2, 3, 4, 5, 6)
+M <- c("A", "B", "C", "D", "E", "F")
+barplot(h,
+    names.arg = M, xlab = "X", ylab = "Y",
+    col = "#00cec9", main = "Chart", border = "#fdcb6e"
+)
+
+dataframe <- data.frame(h = h, M = M)
+View(dataframe)
+ggplot(dataframe, aes(x = M, y = h)) +
+    geom_bar(stat = "identity", fill = "#00cec9", color = "#fdcb6e") +
+    labs(x = "X", y = "Y", title = "Chart")
